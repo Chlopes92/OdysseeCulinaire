eab8d60cbce37db436550bffe1244c64949ce2a1
Cynthia Poiny <cynthia.poiny@gmail.com>
2023-07-22 22:37:38 +0200

    feat/cart add the icons

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -12,9 +12,9 @@ const Counter = (props: QuantityPickerProps) => {
 
     return (
         <div className={style.flex}>
-            <button onClick={remove}> - </button>
+            <button onClick={remove}><img src="image/icons/counter-.png" alt="enlever un produit" /></button>
             <p>{quantity}</p>
-            <button onClick={add}> + </button>
+            <button onClick={add}><img src="image/icons/counter+.png" alt="ajouter un produit" /></button>
         </div>
     )
 }
