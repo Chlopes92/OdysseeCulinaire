44bd61b6ef0371445dbb48a61a654e79f0a27867
POINY Cynthia <cynthia.poiny@renault.com>
2023-08-09 23:19:25 +0200

    style with the cart logo and quantity

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -15,8 +15,11 @@ const Header = () => {
       </Link>
       <NavBar />
       <Link to="/cart">
+        <div className={style.flex}>
         <img className={style.panier} src="/image/icons/shopping-cart.png" alt="Panier" />
-        <p>{totalQuantity}</p>
+        <div className={style.borderQuantity} ><p className={style.quantity}>{totalQuantity}</p></div>
+
+        </div>
       </Link>
     </section>
   );
