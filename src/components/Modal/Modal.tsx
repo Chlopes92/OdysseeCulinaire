89fe7408a08bd88e7f99132ee3f09645b1ae534d
POINY Cynthia <cynthia.poiny@renault.com>
2023-08-10 23:40:27 +0200

    second modal pour le paiement en carte ou especes

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,43 +1,20 @@
-import { useState } from 'react';
 import style from './Modal.module.css';
-import { NavLink } from 'react-router-dom';
 
+interface IModalProps {
+    children: JSX.Element | JSX.Element[];
+}
 
-const Modal = () => {
-    const [modal, setModal] = useState(true);
+const Modal = (props: IModalProps) => {
+    const { children } = props;
 
-    const toggleModal = () => {
-        setModal(!modal);
-    }
-
-return (
-    <div>
-       {modal && 
+    return (
         (<section>
-        <div  className={style.overlay}></div>
-        <div className={style.modalContent}>
-        <button className={style.btnClose} onClick={toggleModal}>X</button>
-            <h3>Choisissez votre mode de commande</h3>
-            <p>Paris - 18eme - 33 rue du Simplon</p>
-            <ul className={style.list}>
-                <li>Click & Collect (jusqu'à 23h30)</li>
-                <li>Service en salle (jusqu'à 00h00)</li>
-                <li>Livraison (jusqu'à 23h30)</li>
-            </ul>
-
-
-
-            <div className={style.link}>
-                <button><img src="image\icons\emporter.png" alt="emporter"/>Emporté</button>
-                <button><img src="image\icons\sur-place.png" alt="sur-place"/>Sur place</button>
-                <button><img src="image\icons\livraison.png" alt="livraison"/><NavLink to='/form'>Livraison</NavLink></button>
+            <div className={style.overlay}></div>
+            <div className={style.modalContent}>
+                {children}
             </div>
-        </div>
         </section>)
-       }
-</div>
-)}
-     
-
+    )
+}
 
 export default Modal;
