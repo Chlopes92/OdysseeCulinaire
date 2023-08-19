5a93a7e5d912a063855fb1ca16881e8080dbfba7
Christelle <christelle.lopes92@gmail.com>
2023-08-19 16:23:36 +0200

    filtre page Category

--- src/pages/ProductCategoryPage.tsx
+++ src/pages/ProductCategoryPage.tsx
@@ -1,13 +1,117 @@
-import { PRODUCTS } from "mocks/products";
+import { PRODUCTS, ProductAllergyType, TagType } from "mocks/products";
 import {
+    useNavigate,
     useParams,
+    useSearchParams,
     // useNavigate 
 } from "react-router-dom";
 import Carousel from "components/Carousel/Carousel";
 import ProductItem from "components/ProductItem/ProductItem";
 import style from './ProductsPage.module.css'
-// import { ProductCategoryType } from "mocks/products";
 
+
+const tagValues: TagType[] = ["vegan", "vegetarien", "viande", "sans-gluten", "avec-alcool", "sans-alcool"];
+const allergyValues: ProductAllergyType[] = ["produits-laitiers", "allium", "poisson", "arachide"];
+
+const TagList = () => {
+const navigate = useNavigate();
+let { category } = useParams();
+const [query] = useSearchParams()
+let tagArray = query.get('filter')?.split(",") as TagType[]
+let allergyArray = query.get('excludeAllergy')?.split(",") as ProductAllergyType[]
+console.log(tagArray)
+if(tagArray === undefined){
+    tagArray=[]
+}else if (tagArray.length && tagArray[0].length === 0){
+    tagArray.pop()
+}
+if(allergyArray === undefined){
+        allergyArray=[]
+    } else if (allergyArray.length && allergyArray[0].length === 0){
+        allergyArray.pop()
+    }
+// console.log("test", test.getAll('filter') )
+    const toggleTag = (tag: TagType) =>{
+        if(tagArray.includes(tag)){
+            tagArray = tagArray.filter(tagValue => {
+                if(tag === tagValue){
+                    return false;
+                }else{
+                    return true;
+                }
+            })
+        } else{
+            tagArray.push(tag)
+            // console.log(tagArray);
+        }
+        navigate({
+            pathname: `/products/category/${category}`,
+            search:`?filter=${tagArray}&excludeAllergy=${allergyArray}`
+        })
+    }
+
+    return (
+        <ul>
+            {tagValues.map(tag => (
+                <li key={tag} className={`${tagArray.includes(tag)? 'selected' : ""}`}>
+                    <button onClick={() => toggleTag(tag)}>
+                        {tag}
+                    </button>
+                </li>
+            ))}
+        </ul>
+    );
+}
+
+const AllergyList = () => {
+    const navigate = useNavigate();
+    const [query] = useSearchParams();
+    let { category } = useParams();
+    let allergyArray = query.get('excludeAllergy')?.split(",") as ProductAllergyType[];
+    let filterArray = query.get('filter')?.split(",") as TagType[]
+    // console.log(allergyArray)
+    if(allergyArray === undefined){
+        allergyArray=[]
+    } else if (allergyArray.length && allergyArray[0].length === 0){
+        allergyArray.pop()
+    }
+    if(filterArray === undefined){
+        filterArray=[]
+    } else if (filterArray.length && filterArray[0].length === 0){
+        filterArray.pop()
+    }
+   
+        const toggleAllergy = (allergy: ProductAllergyType) =>{
+            // console.log(allergy)
+            if(allergyArray.includes(allergy)){
+                allergyArray = allergyArray.filter(allergyValue => {
+                    if(allergy === allergyValue){
+                        return false;
+                    }else{
+                        return true;
+                    }
+                })
+            } else{
+                allergyArray.push(allergy)
+                console.log(allergyArray);
+            }
+            navigate({
+                pathname: `/products/category/${category}`,
+                search:`?filter=${filterArray}&excludeAllergy=${allergyArray}`
+            })
+        }
+    return (
+        <ul>
+            {allergyValues.map((allergy) => (
+                <li key={allergy} className={`${allergyArray.includes(allergy)? 'selected' : ""}`}>
+                    <button onClick={() => toggleAllergy(allergy)}>
+                        {allergy}
+                    </button>
+                </li>
+            ))}
+        </ul>
+    );
+}
 
 
 const ProductCategoryPage = () =>{
@@ -18,10 +122,46 @@ const ProductCategoryPage = () =>{
     console.log(category)
     let product_displayed = PRODUCTS.filter(product => product.category.includes(category ?? ''));
    // let product_displayed = PRODUCTS.filter(product => !product.category.includes(category ?? '')); //Exemple
+//    let product_filtered = PRODUCTS
+   const [query] = useSearchParams()
+   const tagArray = query.get('filter')?.split(",") as TagType[]
+   console.log("Tag Array: ", tagArray);
+   let allergyArray = query.get('excludeAllergy')?.split(",") as ProductAllergyType[]
+   // console.log("Allergy Array: ", allergyArray);
+       if(tagArray?.length && tagArray[0].length){
+        product_displayed = PRODUCTS.filter(product => {
+           let tmp = true
+               tagArray.forEach(tag => {
+                   if (! product.tags?.includes(tag ?? ''))
+                       tmp = false
+               })
+               return tmp;
+               // return product.tags?.includes(tagValues ?? '')
+           });
+       }
+       if(allergyArray?.length && allergyArray[0].length){
+           console.log(allergyArray)
+           product_displayed = product_displayed.filter(product => {
+               let tmp = true
+                   allergyArray.forEach(allergy =>{
+                       if(product.allergy?.includes(allergy)){
+                           tmp = false
+                   }})
+                   return tmp;
+           });
+       }
+
 
     return (
         <section>
             <Carousel />
+            <div>
+                <h2>Filtrage :</h2>
+                <TagList />
+
+                <h2>Allergènes :</h2>
+                <AllergyList />
+            </div>
             <ul className={style.menu}>
                 {product_displayed.map((product) => (
                     <li key={product.id}>
