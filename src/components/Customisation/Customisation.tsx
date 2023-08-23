bac297b20824310ceab46d7ee3d87e604edde682
Lileczka <liliana.guderska@gmail.com>
2023-08-24 01:18:33 +0200

    mock mythologie,checkbox, responsive

--- src/components/Customisation/Customisation.tsx
+++ src/components/Customisation/Customisation.tsx
@@ -5,29 +5,80 @@ import style from './Customisation.module.css';
 interface CustomisationProps {
   product: {
     title: string;
+    description:string;
+    mythologie?: string | null;
     price: number;
     includedIngredients: { ingredient: { id: number; title: string }; isSelected: boolean }[];
     extras: { ingredient: { id: number; title: string }; additionalPrice: number; isSelected: boolean }[];
   };
 }
 
-class Customisation extends Component<CustomisationProps> {
+interface CustomisationState {
+  selectedIngredients: { [key: number]: boolean };
+  selectedExtras: { [key: number]: boolean };
+}
+
+class Customisation extends Component<CustomisationProps, CustomisationState> {
+  constructor(props: CustomisationProps) {
+    super(props);
+    this.state = {
+      selectedIngredients: {},
+      selectedExtras: {},
+    };
+  }
+
+  handleIngredientClick = (ingredientId: number) => {
+    this.setState((prevState) => ({
+      selectedIngredients: {
+        ...prevState.selectedIngredients,
+        [ingredientId]: !prevState.selectedIngredients[ingredientId],
+      },
+    }));
+  };
+
+  handleExtraClick = (extraId: number) => {
+    this.setState((prevState) => ({
+      selectedExtras: {
+        ...prevState.selectedExtras,
+        [extraId]: !prevState.selectedExtras[extraId],
+      },
+    }));
+  };
+
   render() {
     const { product } = this.props;
+    const { selectedIngredients, selectedExtras } = this.state;
+
     return (
       <div className={style.container}>
-        <h2>{product.title}</h2>
+        <h2 className={style.title}>{product.title} *</h2>
+        <p>{product.description}</p>
         <p>{product.price} €</p>
+        <p>{product.mythologie} </p>
         <hr className={style.customHr} />
+        <p className={style.inclus}>Inclus : </p>
         <div className={style.select}>
-        <SelectedProduct />
-        <img src="/image/icons/checkbox.png" alt="ajouter un produit au panier" />
-        </div>
+      </div>
         <div>
-          <p>Included Ingredients:</p>
-          <ul>
+        
+        <ul>
             {product.includedIngredients.map((item) => (
-              <li key={item.ingredient.id}>{item.ingredient.title}</li>
+              <li
+                key={item.ingredient.id}
+                onClick={() => this.handleIngredientClick(item.ingredient.id)}
+              >
+                 <span className={`${style.checkboxIconWrapper}`}>
+                {selectedIngredients[item.ingredient.id] ? (
+                  <img
+                    src="/image/icons/checkbox.png"
+                    alt="ajouter un produit au panier"
+                  />
+                ) : (
+                  <SelectedProduct />
+                )}
+                {item.ingredient.title}
+                </span>
+              </li>
             ))}
           </ul>
         </div>
