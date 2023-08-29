ea971c179bbc40ffb7f9f53e121ef0b8f1d544ca
Cynthia Poiny <cynthia.poiny@gmail.com>
2023-08-29 16:20:46 +0200

    fix bug article panier double et calcul price

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -3,44 +3,51 @@ import { useCartContext } from "contexts/Cart.context";
 import style from "./CartPage.module.css";
 import Button from "components/Button/Button";
 import { NavLink } from "react-router-dom";
-import Modal from "components/Modal/Modal";
-import { useState } from "react";
+import {getTotalPriceWithExtra} from "../../src/contexts/TotalExtraPrice";
 
 const CartPage = () => {
-  const { getTotalPrice, resetCart } = useCartContext();
-  const totalPrice = getTotalPrice();
-  const reset = () => resetCart();
+const { resetCart, products } = useCartContext();
+// const totalPrice = getTotalPrice();
+const reset = () => resetCart();
 
-  return (
-    <main>
-      <section className={style.cartPage}>
-        <h1 className={style.title}>L'Odyssée Culinaire</h1>
+const total = () => { 
+let totalPrice = 0; products.forEach((p)=> {
+totalPrice += (getTotalPriceWithExtra(p.product) * p.quantity) }) 
+console.log(totalPrice); 
+return totalPrice 
+}
 
-        <div className={style.flex}>
-          <p>Panier</p>
-          {totalPrice !== 0 && <button onClick={reset}>Vider le panier</button>}
-        </div>
+return (
+<main>
+<section className={style.cartPage}>
+<h1 className={style.title}>L'Odyssée Culinaire</h1>
 
-        {/* Condition pour afficher votre panier est vide */}
-        {totalPrice == 0 ? (
-          <div>
-            <p className={style.emptyCart}>Votre panier est vide !</p>
-            <NavLink to="/products">
-              <Button title="Retour à la carte" />
-            </NavLink>
-          </div>
-        ) : (
-          <div>
-            <ProductCard />
-            <p className={style.total}>Total: {totalPrice}€ </p>
-            <NavLink to="/payment">
-              <Button title="Payer la commande" />
-            </NavLink>
-          </div>
-        )}
-      </section>
-      </main>
-  );
+<div className={style.flex}>
+<p>Panier</p>
+{total() !== 0 && <button onClick={reset}>Vider le panier</button>}
+</div>
+
+{/* Condition pour afficher votre panier est vide */}
+{total() == 0 ? (
+<div>
+<p className={style.emptyCart}>Votre panier est vide !</p>
+<NavLink to="/products">
+<Button title="Retour à la carte" />
+</NavLink>
+</div>
+) : (
+<div>
+<ProductCard />
+<p className={style.total}>Total: {total()}€ </p>
+<NavLink to="/payment">
+<Button title="Payer la commande" />
+</NavLink>
+</div>
+)}
+</section>
+</main>
+);
 };
 
 export default CartPage;
+
--- src/pages/ProductDetailPage.tsx
+++ src/pages/ProductDetailPage.tsx
@@ -9,107 +9,99 @@ import Button from 'components/Button/Button';
 import { useParams } from 'react-router-dom';
 import { useCartContext } from 'contexts/Cart.context';
 import { redirect } from "react-router-dom";
-
+import {getTotalPriceWithExtra} from "../../src/contexts/TotalExtraPrice";
 
 const ProductDetailPage = () => {
 
-  const [quantity, setQuantity] = useState(1);
-  const { id } = useParams();
-  const { addOne } = useCartContext();
+const [quantity, setQuantity] = useState(1);
+const { id } = useParams();
+const { addOne } = useCartContext();
 
-  const [totalPrice, setTotalPrice] = useState(0);
+const [totalPrice, setTotalPrice] = useState(0);
 
-  // utiliser useState pour pouvoir accéder à la modification d'un élément d'un objet
-  const [p, setP] = useState<IProduct | undefined>();
+// utiliser useState pour pouvoir accéder à la modification d'un élément d'un objet
+const [p, setP] = useState<IProduct | undefined>();
+const [isAdded, setAdded] = useState(false);
 
-  useEffect(() => {
-    // // Convertir l'ID de string à number
-    const productId = parseInt(id!);
+useEffect(() => {
+// // Convertir l'ID de string à number
+const productId = parseInt(id!);
 
-    // // Trouver le produit correspondant par son ID
-    const product = PRODUCTS.find(product => product.id === productId);
-    console.log(product)
-    if (!product) {
-      redirect("/*");
-    } else {
-      setP(product);
-    }
-  }, [])
-
-
-  // on peut exporter cette fonction dans un fichier pour pouvoir l'utiliser partout 
-  const getTotalPriceWithExtra = (product: IProduct) => {
-    const total = product.extras.reduce((acc, curr) => {
-      return !curr.isSelected ? acc : acc += curr.additionalPrice
-    }, product.price);
-    return total;
-  }
-
-  useEffect(() => {
-    if (p) {
-      setTotalPrice(getTotalPriceWithExtra(p))
-    }
-    // console.log("priceExtra de use state", priceExtra)
-  }, [p])
-
-  // Image de la page
-  const AphroditeImage = '/image/icons/aphrodite.png';
-
-
-  const add = () => {
-    setQuantity(quantity + 1);
-  }
-  const remove = () => {
-    if (quantity > 1)
-      setQuantity(quantity - 1);
-  }
-
-
-  // const totalPriceWithExtra = ((p!.price + priceExtra) * quantity).toFixed(2);
+// // Trouver le produit correspondant par son ID
+const product = PRODUCTS.find(product => product.id === productId);
+console.log(product)
+if (!product) {
+redirect("/*");
+} else {
+setP(product);
+}
+}, [])
 
 
 
+useEffect(() => {
+if (p) {
+setTotalPrice(getTotalPriceWithExtra(p))
+}
+}, [p])
+
+// Image de la page
+const AphroditeImage = '/image/icons/aphrodite.png';
+
+const add = () => {
+setQuantity(quantity + 1);
+}
+const remove = () => {
+if (quantity > 1)
+setQuantity(quantity - 1);
+}
+
+const addToBasket = () => {
+p!.isAddToCart = true;
+addOne(p!, quantity)
+console.log("produit", p!)
+}
 
 
-  return (
-    <section>
-      {p && (
-        <>
-          <div className={style.containerText}>
-            <Link to="/products">
-              <p>&lt; Revenir à la carte</p>
-            </Link>
-            <div className={style.container}>
-              <div className={style.containerImg}>
-                <img className={style.imageResponsive} src={p!.img.src} alt={p!.img.alt} />
+return (
+<section>
+{p && (
+<>
+<div className={style.containerText}>
+<Link to="/products">
+<p>&lt; Revenir à la carte</p>
+</Link>
+<div className={style.container}>
+<div className={style.containerImg}>
+<img className={style.imageResponsive} src={p!.img.src} alt={p!.img.alt} />
 
-                <img className={style.aphroditeImage} src={AphroditeImage} alt={AphroditeImage} />
-              </div>
+<img className={style.aphroditeImage} src={AphroditeImage} alt={AphroditeImage} />
+</div>
 
-              <Customisation p={p!} setP={setP} />
+<Customisation p={p!} setP={setP} />
 
-            </div>
-          </div>
+</div>
+</div>
 
-          <div className={style.containeurBas}>
+<div className={style.containeurBas}>
 
-            <Counter quantity={quantity}
-              add={() => add()}
-              remove={() => remove()}
-            />
-            <div className={style.buttonAdd}>
-              <NavLink to="/cart">
-                <Button
-                  title={`Ajouter au Panier - ${totalPrice * quantity} €`}
-                  onClick={() => addOne(p!, quantity)}
-                />
-              </NavLink>
-            </div>
-          </div>
-        </>
-      )}
-    </section>
-  );
+<Counter quantity={quantity}
+add={() => add()}
+remove={() => remove()}
+/>
+<div className={style.buttonAdd}>
+<NavLink to="/cart">
+<Button
+title={`Ajouter au Panier - ${totalPrice * quantity} €`}
+onClick={() => addToBasket()}
+/>
+</NavLink>
+</div>
+</div>
+</>
+)}
+</section>
+);
 };
 
 export default ProductDetailPage;
